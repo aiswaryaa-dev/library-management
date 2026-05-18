05bfd3a5cf0c16c031ce57cf35391e68663c9bf4
aiswaryaa-dev <aiswaryaadev3@gmail.com>
2026-05-18 22:46:31 +0530

    library management

---
+++ main.py
@@ -0,0 +1,8 @@
+def otp():
+    pwd=3334444
+    msg="your otp is"+str(pwd)+".don't share with anyone"
+    return msg
+def activation():
+    link='aaaaaa'
+    msg="click the below link to activate. \n"+link
+    return msg
